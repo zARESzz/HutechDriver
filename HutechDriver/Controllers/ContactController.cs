bfac5c857d53d2f0b7bba624cc230ce3bf6cce12
Pham Hung <phamthehung1912@gmail.com>
2023-07-01 00:42:58 +0700

    UploadImageTuyenDung

--- HutechDriver/Controllers/ContactController.cs
+++ HutechDriver/Controllers/ContactController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using HutechDriver.Models;
 using HutechDriver.Models.EF;
 
@@ -19,18 +21,38 @@ namespace HutechDriver.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Index(Contact model)
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Contact model, HttpPostedFileBase ImageFile, HttpPostedFileBase ImageFile1)
         {
+            if (ImageFile != null && ImageFile.ContentLength > 0 && ImageFile1 != null && ImageFile1.ContentLength > 0)
+            {
+                // Lưu tệp đã tải lên vào thư mục trên máy chủ
+                var fileName1 = Path.GetFileName(ImageFile.FileName);
+                var filePath1 = Path.Combine(Server.MapPath("~/Content/ImagesAcc"), fileName1);
+                ImageFile.SaveAs(filePath1);
+                model.ImagePeople = fileName1;
+
+                var fileName2 = Path.GetFileName(ImageFile1.FileName);
+                var filePath2 = Path.Combine(Server.MapPath("~/Content/ImagesAcc"), fileName2);
+                ImageFile1.SaveAs(filePath2);
+                model.ImagePapers = fileName2;
+            }
             if (ModelState.IsValid)
             {
+
                 model.IsRead = 0;
                 model.IsStatus = 0;
                 model.CreateDate = DateTime.Now;
                 model.ModifiedDate = DateTime.Now;
+
                 db.Contacts.Add(model);
-                db.SaveChanges();              
+                db.SaveChanges(); 
+                
+                // Chuyển hướng hoặc hiển thị thông báo thành công
+               
             }
-            return View();
+
+            return View(model);
         }
     }
 }
